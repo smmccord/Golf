9903cebb0e2beed3562e3c86ac7c07da95232d78
Sean McCord <mccordsean1@aol.com>
2024-02-07 00:51:28 -0800

    Initial Commit

---
+++ Golf/GolfApp.swift
@@ -0,0 +1,17 @@
+//
+//  GolfApp.swift
+//  Golf
+//
+//  Created by Sean McCord on 2/7/24.
+// created
+
+import SwiftUI
+
+@main
+struct GolfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
